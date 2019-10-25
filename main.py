0f04bcf0c5d26b10d65b8da9dc47345625f69f64
solanav <solanav@qq.com>
2019-10-25 15:40:37 +0200

    Added config

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import json
 
-class config():
-    def __init__(file_name):
-        self.file_name = file_name
+class Check():
+
+class Config():
+    def __init__(self, file_name):
         try:
-            self.file = open(file_name, 'w')
-        except:
-            raise ValueError('Failed to open config file')
+            self.file = open(file_name, 'r+')
+        except FileNotFoundError:
+            raise ValueError('Failed to find config file')
+        
+        self.file_name = file_name
 
-    def parse_config():
+        self.load_options()
 
-
+    def load_options(self):
+        self.data = json.load(self.file)
+    
+    def script_dir(self):
+        return self.data['general']['script_dir']
 
 def main():
+    main_config = Config("config.json")
+    print(main_config.data)
 
 if __name__ == '__main__':
     main()
