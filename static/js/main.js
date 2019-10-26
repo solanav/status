8b0d71ab6d783bfdadbfaaa2146b9a98dd836ade
solanav <solanav@qq.com>
2019-10-26 23:53:08 +0200

    JS added

---
+++ static/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/');
+
+    socket.on('update_status', function(data) {
+        state = document.getElementById(data.name);
+        state.innerHTML = data.status;
+        if (data.status == 'ERROR') {
+            state.style.color = 'red';
+        } else {
+            state.style.color = 'green';
+        }
+    });
+});
+
+(function(){
+    socket.emit('reload');
+    setTimeout(arguments.callee, 1000);
+})();
